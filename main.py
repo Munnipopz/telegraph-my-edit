04da3706504b53362ba487e898786c3d0e6d21ce
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-22 22:31:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ TGraph = Client(
 @TGraph.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text(
-        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by  @Munnipopz https://t.me/c/1334101352/2 ",
+        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by  @Munnipopz https://telegra.ph/file/6f8d9001de13b3f8e573a.jpg ",
         True,
         
     )
