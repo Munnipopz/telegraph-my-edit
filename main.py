d5419b254bb4951dce0e76e676e36bf0714f957b
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-25 23:21:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os
 import uuid
 import shutil
 import logging
-import pyrogram
-
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
-from pyrogram.errors import UserNotParticipant
+from creds import Credentials
+from telegraph import upload_file
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+logging.basicConfig(level=logging.INFO)
 
 
 TGraph = Client(
@@ -16,36 +18,23 @@ TGraph = Client(
     api_hash=Credentials.API_HASH,
 )
 
-@Client.on_message(filters.command(["start"]))
-def send_start(bot, update):
-    # logger.info(update)
-    
-    bot.send_message(
-        chat_id=update.chat.id,
-        text=script.START_TEXT.format(update.from_user.first_name),
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
 
-
-
-
-@TGraph.on_message(filters.photo)
-async def getimage(client, message):
-    tmp = os.path.join("downloads", str(message.chat.id))
-    if not os.path.isdir(tmp):
-        os.makedirs(tmp)
-    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
-    dwn = await message.reply_text("Downloading...", True)
-    img_path = await client.download_media(message=message, file_name=img_path)
-    await dwn.edit_text("Uploading...")
-    try:
-        response = upload_file(img_path)
-    except Exception as error:
-        await dwn.edit_text(f"Oops something went wrong\n{error}")
-        return
-    await dwn.edit_text(f"https://telegra.ph{response[0]}")
-    shutil.rmtree(tmp, ignore_errors=True)
-
-
-TGraph.run()
+@Client.on_message(filters.command(["test"]))
+async def test(client, message):
+        await message.reply_text(
+            text="{text}",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Button 1", url="{link}"),
+                        InlineKeyboardButton("Button 2", url="{link}"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Button 3", url="{link}")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
