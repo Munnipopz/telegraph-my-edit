ab454fa6f7360854d9eabb5181f19b9f42f428ff
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-25 16:00:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-import uuid
-import shutil
-import logging
-from pyrogram import Client, filters
-from creds import Credentials
-from telegraph import upload_file
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,31 +13,30 @@ TGraph = Client(
 )
 
 
-@TGraph.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text(
-        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by  @Munnipopz https://telegra.ph/file/6f8d9001de13b3f8e573a.jpg ",
-        True,
-        
-    )
+@Client.on_message(filters.command(["Start"])
+        await message.reply_text(
+            text="{പൊളി}",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Button 1", url="{https://t.me/mpazaan
 
 
-@TGraph.on_message(filters.photo)
-async def getimage(client, message):
-    tmp = os.path.join("downloads", str(message.chat.id))
-    if not os.path.isdir(tmp):
-        os.makedirs(tmp)
-    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
-    dwn = await message.reply_text("Downloading...", True)
-    img_path = await client.download_media(message=message, file_name=img_path)
-    await dwn.edit_text("Uploading...")
-    try:
-        response = upload_file(img_path)
-    except Exception as error:
-        await dwn.edit_text(f"Oops something went wrong\n{error}")
-        return
-    await dwn.edit_text(f"https://telegra.ph{response[0]}")
-    shutil.rmtree(tmp, ignore_errors=True)
+}"),
+                        InlineKeyboardButton("Button 2", url="{https://t.me/mpazaan
 
 
-TGraph.run()
+}"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Button 3", url="{https://t.me/mpazaan
+
+
+}")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
