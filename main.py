67a85aa376d872ea6ec3f5c42a34659a4d4d4eee
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-25 16:14:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,23 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
             ),
             reply_to_message_id=message.message_id
         )
+
+@TGraph.on_message(filters.photo)
+async def getimage(client, message):
+    tmp = os.path.join("downloads", str(message.chat.id))
+    if not os.path.isdir(tmp):
+        os.makedirs(tmp)
+    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
+    dwn = await message.reply_text("Downloading...", True)
+    img_path = await client.download_media(message=message, file_name=img_path)
+    await dwn.edit_text("Uploading...")
+    try:
+        response = upload_file(img_path)
+    except Exception as error:
+        await dwn.edit_text(f"Oops something went wrong\n{error}")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    shutil.rmtree(tmp, ignore_errors=True)
+
+
+TGraph.run()
