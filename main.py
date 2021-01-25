fbfd8d2481096fbcc61d268d24f85e6f306803d1
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-25 23:39:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,15 +16,6 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@TGraph.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text(
-        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by  @Munnipopz https://telegra.ph/file/6f8d9001de13b3f8e573a.jpg ",
-        True,
-        
-    )
-
-
 TGraph = Client(
     "Image upload bot",
     bot_token=Credentials.BOT_TOKEN,
@@ -33,6 +24,13 @@ TGraph = Client(
 )
 
 
+@TGraph.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text(
+        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by  @Munnipopz https://telegra.ph/file/6f8d9001de13b3f8e573a.jpg ",
+        True,
+        
+    )
 @Client.on_message(filters.command(["test"]))
 async def test(client, message):
         await message.reply_text(
