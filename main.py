709dfdad6e98b76e9efaf9190d2878f503caad27
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-25 16:07:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import os
-import uuid
-import shutil
-import logging
-from pyrogram import Client, filters
-from creds import Credentials
-from telegraph import upload_file
-
-logging.basicConfig(level=logging.INFO)
-
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -31,39 +21,3 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
             ),
             reply_to_message_id=message.message_id
         )
-TGraph = Client(
-    "Image upload bot",
-    bot_token=Credentials.BOT_TOKEN,
-    api_id=Credentials.API_ID,
-    api_hash=Credentials.API_HASH,
-)
-
-
-@TGraph.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text(
-        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by  @Munnipopz https://telegra.ph/file/6f8d9001de13b3f8e573a.jpg ",
-        True,
-        
-    )
-
-
-@TGraph.on_message(filters.photo)
-async def getimage(client, message):
-    tmp = os.path.join("downloads", str(message.chat.id))
-    if not os.path.isdir(tmp):
-        os.makedirs(tmp)
-    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
-    dwn = await message.reply_text("Downloading...", True)
-    img_path = await client.download_media(message=message, file_name=img_path)
-    await dwn.edit_text("Uploading...")
-    try:
-        response = upload_file(img_path)
-    except Exception as error:
-        await dwn.edit_text(f"Oops something went wrong\n{error}")
-        return
-    await dwn.edit_text(f"https://telegra.ph{response[0]}")
-    shutil.rmtree(tmp, ignore_errors=True)
-
-
-TGraph.run()
