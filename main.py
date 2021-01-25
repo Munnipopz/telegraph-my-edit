69ebeb81bc55a13637c5faa1f2292ed28b9901d8
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-25 21:38:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,23 +19,24 @@ TGraph = Client(
 )
 
 
-@Client.on_message(filters.command(["test"]))
-    await message.reply_text(
-      text="{text}",
-      disable_web_page_preview=True,
-      reply_markup=InlineKeyboardMarkup(
-      [
-      [
-      InlineKeyboardButton("Button 1", url="{link}"),
-      InlineKeyboardButton("Button 2", url="{link}")
-      ],
-      [
-      InlineKeyboardButton("Button 3", url="{link}")
-      ]
-      ]
-      ),
-    reply_to_message_id=message.message_id
-    )
+@Client.on_message(filters.command(["test"]) )
+       await message.reply_text(
+            text="{text}",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Button 1", url="{link}"),
+                        InlineKeyboardButton("Button 2", url="{link}"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Button 3", url="{link}")
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
 
 
 @TGraph.on_message(filters.photo)
