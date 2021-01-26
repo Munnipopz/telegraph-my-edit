a7ba7c727f6be9376b9a6fe81df48bc6f6202788
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-26 22:56:34 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,11 @@
+from creds import Config
+
+class Translation(object):
+    START_TEXT = """Hello <i><b>{}</b></i>,
+
+This is a Telegram Rename Bot Devloped <a href='https://t.me/TeleBotinC'>TeleBots</a> by {}
+
+I can rename ✍ your files with custom thumbnail and upload as video/file as directed.
+
+Type /help now for more details."""
+    
